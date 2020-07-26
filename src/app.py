62f8840db420aacc8bfccb5e429c6e77e611fb38
randerson <randerson_d@hotmail.com>
2020-07-25 21:03:37 -0300

    adjust in the app.py file to import the controller and configure the CORS library, to allow header information to be sent

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .config import app_config
-
+from .models import db, bcrypt
+from flask_cors import CORS
+from .controllers.ContactController import contact_api as contact_blueprint
 
 
 def create_app(env_name):
@@ -10,10 +12,13 @@ def create_app(env_name):
   
   # app initiliazation
   app = Flask(__name__)
-
+  cors = CORS(app, resources={r"/api/v1/contacts*": {"origins": "*"}})
+  cors = CORS(app, resources={r"/api/v1/contacts/edit/<int:contact_id>*": {"origins": "*"}})
 
   app.config.from_object(app_config[env_name])
 
+  # initializing bcrypt
+  # bcrypt.init_app(app) 
 
   db.init_app(app) 
 
